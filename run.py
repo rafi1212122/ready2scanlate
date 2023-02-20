0bf8ff7c26a1f9ff1a6d3a8b58a9490902465917
rafi1212122 <rafi1212122@outlook.com>
2023-02-20 16:54:30 +0700

    model args

--- run.py
+++ run.py
@@ -2,14 +2,19 @@ import argparse
 from utils.json_writer import JsonOcr, write_json
 import ws
 
-ocr_model = 'models/manga-ocr-base'
-detector_model = 'models/comictextdetector.pt'
+ocr_models = { 
+    'manga_ocr': r'models/manga-ocr-base',
+    'ocr_48px_ctc': r'models/ocr-ctc.ckpt',
+}
+detector_model = r'models/comictextdetector.pt'
+
+inpainting_model = r'models/inpainting_lama_mpe.ckpt'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str, help="input file")
-parser.add_argument("-o", "--output", default='results',
+parser.add_argument("-o", "--output", default=r'tmp/results',
                     type=str, help="output directory")
-parser.add_argument("--ocr-model", default=ocr_model,
+parser.add_argument("--ocr-model", default='manga_ocr',
                     type=str, help="change directory of ocr model")
 parser.add_argument("--detector-model", default=detector_model,
                     type=str, help="change directory of detector model")
@@ -17,6 +22,8 @@ parser.add_argument("-m", "--mode", help="select run mode", choices=["cli", "ws"
 parser.add_argument("--ws-port", help="specify websocket port (default: 5005)", type=int, default=5005)
 
 args = parser.parse_args()
+
+ocr_model = ocr_models[args.ocr_model]
 
 if args.mode=="cli":
     json_ocr = JsonOcr(ocr_model, detector_model)
